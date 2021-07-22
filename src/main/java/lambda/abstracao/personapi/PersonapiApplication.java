30085b306cd323130019bbcfaca7b2a2600e620c
pedro-git-projects <pedro.coding.contact@gmail.com>
2021-07-22 13:02:32 -0300

    created all classes in main

--- src/main/java/lambda/abstracao/personapi/PersonapiApplication.java
+++ src/main/java/lambda/abstracao/personapi/PersonapiApplication.java
@@ -1,7 +1,7 @@
 package lambda.abstracao.personapi;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+		import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PersonapiApplication {
